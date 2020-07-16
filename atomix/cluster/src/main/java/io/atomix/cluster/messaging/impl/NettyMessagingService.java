d46a70acf15d6f9ed8aa242ab5a417548a81380f
pihme <pihme@users.noreply.github.com>
2020-07-16 17:30:59 +0200

    chore(ci): change logging config

--- atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
+++ atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
@@ -691,14 +691,11 @@ public class NettyMessagingService implements ManagedMessagingService {
                       serverChannel = f.channel();
                       bind(bootstrap, ifaces, port, future);
                     } else {
-                      System.out.println(
-                          "Failed to bind TCP server to port {"
-                              + iface
-                              + " }:{"
-                              + port
-                              + "} due to {"
-                              + f.cause()
-                              + "}");
+                      log.warn(
+                          "Failed to bind TCP server to port {}:{} due to {}",
+                          iface,
+                          port,
+                          f.cause());
                       future.completeExceptionally(f.cause());
                     }
                   });
